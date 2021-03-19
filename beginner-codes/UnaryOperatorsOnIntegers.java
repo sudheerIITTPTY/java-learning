ff24b25ca8a4b66bcafe9dff43148242b186c47b
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-03-19 20:43:04 +0530

    file to practice uniaryopertors on integers is added to repositary

---
+++ beginner-codes/UnaryOperatorsOnIntegers.java
@@ -0,0 +1,19 @@
+/*
+ *  Author : DSK
+ *  This program is to practice unary operators on integers
+ *
+ *
+ * */
+public class UnaryOperatorsOnIntegers {
+    public static void main(String[] args){
+        // declaring integer with initial value as 10
+        int num = 10;
+
+        //printing number
+        System.out.println("Number : " +num);
+
+        // printing num value while updating using both post as well as pre increment
+        System.out.println("printing while post increment the number : " + num++);
+        System.out.println("printing while pre increment value of num " + ++num);
+    }
+}
