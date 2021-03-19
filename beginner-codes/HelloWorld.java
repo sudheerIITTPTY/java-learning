6f102be2141773c18a6247f48839d683ccbf4efb
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-03-19 16:45:18 +0530

    HelloWorld class for printing ...Hello World... statement is created and added to begineer-codes of java-learning reposiory

---
+++ beginner-codes/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+	Author : DSK
+	This program is written to print ...Hello World... in java programming language.
+	
+*/
+//Hello world class
+public class HelloWorld{
+	// main method
+	public static void main(String[] args){
+		//printing Hello world
+		System.out.println("...Hello World...");
+	}
+}
