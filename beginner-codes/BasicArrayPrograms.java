59d7af21b6bb8c7723bb280d5f94ea86dda58a3c
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-03-20 16:11:04 +0530

    Created BasicArrayPrograms and BasicArrayProgramsDriver progrmas in java for work with array

---
+++ beginner-codes/BasicArrayPrograms.java
@@ -0,0 +1,38 @@
+public class BasicArrayPrograms {
+
+    int[] arr;
+    int[] freqs;
+    //initializing arr attribute by copying input array
+    public BasicArrayPrograms(int[] input_array){
+        this.arr = new int[input_array.length];
+        for(int i = 0;i< arr.length;i++)
+            arr[i] = input_array[i];
+    }
+
+    // method for printing arr array in BasicArrayPrograms class
+    public void printArray(){
+        for(int i = 0;i<this.arr.length;i++)
+            System.out.print(this.arr[i]+" ");
+        System.out.println("");
+    }
+
+    //overloaded method for printing array by taking input an array
+    public void printArray(int[] arr){
+        for(int i = 0;i<arr.length;i++)
+            System.out.println(arr[i]+" ");
+    }
+
+    //calculating frequencies of elements in the array
+    public int[] getFreqs(){
+        int noOfElements = this.arr.length;
+        freqs = new int[noOfElements];
+        for(int i = 0;i<noOfElements;i++)
+            freqs[arr[i]]++;
+        return freqs;
+    }
+    //printing frequency array
+    public void printFreqs(){
+        for(int i = 0;i<this.freqs.length;i++)
+            System.out.println(i + " : " +freqs[i]);
+    }
+}
---
+++ beginner-codes/BasicArrayProgramsDriver.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class BasicArrayProgramsDriver {
+    public static void main(String[] args){
+        //scanner object for reading input from user
+        Scanner read = new Scanner(System.in);
+
+        //printing message for user for entering no of elements
+        System.out.print("Enter no of elements  in the array : ");
+
+        //reading no of elements from user using created scanner input
+        int noOfElements = read.nextInt();
+
+        //creating inputArray with input size by the user
+        int[] inputArray = new int[noOfElements];
+
+        //reading and storing elements in the created array
+        for(int i = 0;i<noOfElements;i++)
+            inputArray[i] = read.nextInt();
+
+        //creating object for Basic Array Programs class
+        BasicArrayPrograms obj = new BasicArrayPrograms(inputArray);
+        obj.printArray();
+        obj.getFreqs();
+        obj.printFreqs();
+    }
+}
