8e3aa09eba17aca6c5acf2c035ea5bd4ba59bf31
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-03-23 00:22:59 +0530

    two variations of bubblesort algo for sorting array in ascending and descending order is added to the sorting functions class from sorting-programs branch

--- beginner-codes/SortingFunctions.java
+++ beginner-codes/SortingFunctions.java
@@ -45,4 +45,28 @@ public class SortingFunctions {
             this.arr[j+1] = key;
         }
     }
+
+    public void bubbleSortAscendingOrder(){
+        for(int i = 0;i<this.arr.length-1;i++){
+            for(int j = 0;j<this.arr.length-i-1;j++){
+                if(this.arr[j]>this.arr[j+1]){
+                    int temp = this.arr[j];
+                    this.arr[j] = this.arr[j+1];
+                    this.arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public void bubbleSortDescendingOrder(){
+        for(int i = 0;i<this.arr.length-1;i++){
+            for(int j = 0;j<this.arr.length-1-i;j++){
+                if(this.arr[j]<this.arr[j+1]){
+                    int temp = this.arr[j];
+                    this.arr[j] = this.arr[j+1];
+                    this.arr[j+1] = temp;
+                }
+            }
+        }
+    }
 }
--- beginner-codes/SortingFunctionsDriver.java
+++ beginner-codes/SortingFunctionsDriver.java
@@ -6,10 +6,12 @@
  * */
 public class SortingFunctionsDriver {
     public static void main(String[] args){
-        int[] arr = new int[]{0,1,2,3,4,5,6};
+        int[] arr = new int[]{1,2,3,4,5,6};
         SortingFunctions obj = new SortingFunctions(arr);
         obj.printArray();
-        obj.insertionSortDescendingOrder();
+        obj.bubbleSortDescendingOrder();
+        obj.printArray();
+        obj.bubbleSortAscendingOrder();
         obj.printArray();
     }
 }
