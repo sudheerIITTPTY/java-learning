d62bd5dad9401ea9639fabb39b79814799d9bf2e
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-03-28 23:07:36 +0530

    Simple class Animal program for understanding inhertance in java is added to repositary and Multiple inhertance is created Dog class is created by extending Animal class and BabyDog1 class is created using Dog class.

---
+++ beginner-codes/Animal.java
@@ -0,0 +1,17 @@
+/*
+ *  Author : DSK
+ *  This program is for testing inheritance in java and vehicle is base class for
+ *
+ *
+ * */
+class Animal{
+    void eat(){System.out.println("animal is eating...");}
+}
+class Dog extends Animal{
+    void eat(){System.out.println("dog is eating...");}
+}
+class BabyDog1 extends Dog{
+    public static void main(String args[]){
+        Animal a=new BabyDog1();
+        a.eat();
+    }}
